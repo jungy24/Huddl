cca596358221e108fca79455183b13ca794b34ab
Your Full Name <jungy24@gmail.com>
2017-05-25 23:03:49 -0400

    Add side bar menu with animation

---
+++ Huddl/SubMainViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SubMainViewController.swift
+//  Huddl
+//
+//  Created by Jungyoon Yu on 5/25/17.
+//  Copyright © 2017 Jungyoon Yu. All rights reserved.
+//
+
+import UIKit
+
+class SubMainViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var chatButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        sideMenus()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func sideMenus() {
+        
+        if revealViewController() != nil{
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            //revealViewController().rightViewRevealWidth = 160
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
